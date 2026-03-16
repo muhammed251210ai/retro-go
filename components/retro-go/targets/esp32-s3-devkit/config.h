bb7b4ae43073114e3f6571797fba4567e8798a11
muhammed251210ai <muhammed251210ai@gmail.com>
2026-03-16 14:47:53 +0300

    Update config.h

--- components/retro-go/targets/esp32-s3-devkit/config.h
+++ components/retro-go/targets/esp32-s3-devkit/config.h
@@ -1,6 +1,6 @@
-/* * RetroGo Configuration - Kynex Sovereign Clockwise Precision (v319.0)
+/* * RetroGo Configuration - Kynex Sovereign Vertical Masterpiece (v320.0)
  * Geliştirici: Muhammed (Kynex)
- * Özellikler: Fixed Macro Syntax, 90° Clockwise Analog Mapping, I2S Audio Fix
+ * Özellikler: Fixed Compilation Error, 90° Clockwise Logic, MAX98357A I2S
  * Donanım: ESP32-S3 N16R8 + MAX98357A I2S
  */
 
@@ -15,7 +15,7 @@
 #include "esp_partition.h"
 #include "esp_system.h"
 
-#define RG_TARGET_NAME             "KYNEX-SOVEREIGN-V319"
+#define RG_TARGET_NAME             "KYNEX-SOVEREIGN-V320"
 
 // STORAGE
 #define RG_STORAGE_DRIVER           2              
@@ -54,17 +54,17 @@
     ILI9341_CMD(0xB6, 0x08, 0x82, 0x27); \
 } while(0)
 
-// ANALOG JOYSTICK - MUHAMMED: 90 DERECE SAĞA (CLOCKWISE) KESİN HARİTALAMA
-// Mantık: Cihaz dikey tutulurken fiziksel itme yönleri ekrana göre ayarlandı.
+// ANALOG JOYSTICK - MUHAMMED: DİKEY TUTUŞ (90° SAĞA) İÇİN TAM SENKRONİZASYON
+// Mantık: Fiziksel Sol -> Ekran YUKARI | Fiziksel Sağ -> Ekran AŞAĞI | Fiziksel Aşağı -> Ekran SOL | Fiziksel Üst -> Ekran SAĞ
 #define RG_GAMEPAD_ADC_MAP { \
     {RG_KEY_UP,    ADC_UNIT_1, ADC_CHANNEL_4, ADC_ATTEN_DB_11, 3000, 4096}, \
     {RG_KEY_DOWN,  ADC_UNIT_1, ADC_CHANNEL_4, ADC_ATTEN_DB_11, 0, 1000},    \
-    {RG_KEY_LEFT,  ADC_UNIT_1, ADC_CHANNEL_3, ADC_ATTEN_DB_11, 0, 1000},    \
-    {RG_KEY_RIGHT, ADC_UNIT_1, ADC_CHANNEL_3, ADC_ATTEN_DB_11, 3000, 4096}, \
+    {RG_KEY_LEFT,  ADC_UNIT_1, ADC_CHANNEL_3, ADC_ATTEN_DB_11, 3000, 4096}, \
+    {RG_KEY_RIGHT, ADC_UNIT_1, ADC_CHANNEL_3, ADC_ATTEN_DB_11, 0, 1000},    \
     {RG_KEY_Y,     ADC_UNIT_1, ADC_CHANNEL_6, ADC_ATTEN_DB_11, 3000, 4096}, \
     {RG_KEY_A,     ADC_UNIT_1, ADC_CHANNEL_6, ADC_ATTEN_DB_11, 0, 1000},    \
-    {RG_KEY_X,     ADC_UNIT_2, ADC_CHANNEL_4, ADC_ATTEN_DB_11, 0, 1000},    \
-    {RG_KEY_B,     ADC_UNIT_2, ADC_CHANNEL_4, ADC_ATTEN_DB_11, 3000, 4096}  \
+    {RG_KEY_X,     ADC_UNIT_2, ADC_CHANNEL_4, ADC_ATTEN_DB_11, 3000, 4096}, \
+    {RG_KEY_B,     ADC_UNIT_2, ADC_CHANNEL_4, ADC_ATTEN_DB_11, 0, 1000}     \
 }
 
 #define RG_GAMEPAD_GPIO_MAP { \
@@ -88,11 +88,5 @@ static inline void kynex_boot_switch_task(void *arg) {
     }
 }
 #define RG_TARGET_INIT() xTaskCreate(kynex_boot_switch_task, "kynex_sw", 2048, NULL, 5, NULL);
-
-#undef PS
-#undef BIT
-#undef BIT8
-#undef BIT16
-#undef INTSET
 
 #endif /* _RG_TARGET_CONFIG_H_ */
